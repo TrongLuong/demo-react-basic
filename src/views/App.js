cf4df93ecce5219fdd9e03bfabb2cfe27bb1a9b8
MACBOOK PRO <trong-lnd@MacBook-Pro-cua-MACBOOK.local>
2022-06-19 16:16:33 +0700

    update 1

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
-import './App.css';
-import Cnit1 from './footer/index';
+import './App.scss';
+
+
 
 function App() {
  return (
  <div>
-   <Cnit1/>
+   hellogvv
  </div>
   
  );
