f4b12cbe307d9992487258010fbf2ced09ba5eca
MACBOOK PRO <trong-lnd@MacBook-Pro-cua-MACBOOK.local>
2022-06-19 13:36:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import './App.css';
+import Cnit1 from './footer/index';
+
+function App() {
+ return (
+ <div>
+   <Cnit1/>
+ </div>
+  
+ );
+
+  
+ 
+};
+
+export default App;
---
+++ src/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const index1 =()=>{
+
+  return(
+    <div>
+   <p>Hello</p>
+   </div>
+   );
+   
+
+};
+
+
+export default index1;
